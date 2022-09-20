2b220bf321b52c2c2d0a7c63da4b9befe4784975
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-09-20 16:14:53 +0530

    Modifing file and added pratices set for function,and loop

---
+++ expression and operate in js.js
@@ -0,0 +1,42 @@
+// var arr = [2, 3, 4, 5, 6];
+// var dist = { Rutvik: 99, golu: 65, ashish: 96 }
+// for (let a in dist) {
+//     var len = 20 - a.length
+
+//     console.log(a, '-'.repeat(len), ">", dist[a])
+// }
+
+/* 
+ ------------------------------------| while loop in JavaScripts |---------------------------------------------------
+*/
+
+// var age = 15;
+// var ind = 0;
+
+// while (ind < age) {
+//     console.log("The value of age is : ", ind);
+//     ind += 1;
+// }
+
+/* 
+ ------------------------------------| Do while loop in JavaScripts |---------------------------------------------------
+*/
+
+// var age = 15;
+// var ind = 0;
+
+// do {
+//     ind += 1;
+//     console.log("The value of age is : ", ind);
+// } while (ind < age)
+
+/* 
+ ------------------------------------|  Function in JavaScripts |---------------------------------------------------
+*/
+
+function iWantedTORun(name) {
+    console.log(name, ",You are born for win ");
+}
+for (let i = 0; i < 10; i++) {
+    iWantedTORun("Rutvik");
+}
