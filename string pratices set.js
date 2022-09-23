35b65f4f931a428fabc4f1bc5af051b6648b7409
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-09-23 15:36:35 +0530

    Adding string pratices.js file into the directory

---
+++ string pratices set.js
@@ -0,0 +1,12 @@
+var title = "Rutvik Jaiswal is brand 1000"
+
+// console.log(title.includes(1000))
+
+// console.log(title.toLowerCase())
+
+// console.log(title.slice(-4))
+
+title.split("")[4] = 4
+
+title = title.join("");
+console.log(title)
