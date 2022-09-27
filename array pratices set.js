9190016dfb27579e01c4c4a448a0c4d7471f152c
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-09-27 16:09:17 +0530

    Adding [array pratices set.js] file into the directory

---
+++ array pratices set.js
@@ -0,0 +1,44 @@
+let array = [2, 3, 4, 50, 10, 45, 60]
+
+// Q1. Adding number in the array
+// var num = prompt("Enter number you want .");
+// array.push(num);
+
+// Q2. Adding number in the array until number greater than 0
+// while(num>0){
+//   num = prompt("Enter number you want .");
+//   array.push(num);
+// }
+
+// console.log(array);  
+
+
+// Q3. Filter function
+// let newArray = array.filter((val)=>{
+//   if(val%10==0){
+//     return true;
+//   }
+//   return false;
+// })
+
+// console.log(newArray)
+
+
+// Q4. Square of each number in the array  
+
+// let squareArray = array.map((val)=>{
+//   return val*val
+// })
+
+// console.log(squareArray)
+
+
+// Q5. factorial of n natural number from the array
+
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+
+// let arrFact = arr.reduce((val,val2)=>{
+//   return val*val2
+// })
+
+// console.log(arrFact)
