df971ba6635896ceb5e498f3e564ca29cca2ecff
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-10-01 14:21:19 +0530

    Adding pratices set 6

---
+++ pratice set on prompt confirm and alert.js
@@ -0,0 +1,61 @@
+// Pratice set 1
+
+// var age = prompt("Enter your age ? ");
+// if (age > 18) {
+//   alert("You can drive this car")
+// }
+// else {
+//   alert("You can'nt drive this car .")
+// }
+
+
+
+// Pratice set 2
+
+// do {
+//   var age = prompt("Enter your age ? ");
+//   if (age > 18) {
+//     alert("You can drive this car");
+//   }
+//   else {
+//     alert("You can'nt drive this car .");
+//   }
+
+//   var userInput = confirm("Did you want see me again ");
+
+// } while (userInput == true)
+
+
+
+// pratice set 3
+
+// do {
+//   var age = prompt("Enter your age ? ");
+//   if (age < 0) {
+//     console.error("age is not possible in negative");
+//     break
+//   }
+//   if (age > 18) {
+//     alert("You can drive this car");
+//   }
+//   else {
+//     alert("You can'nt drive this car .");
+//   }
+
+//   var userInput = confirm("Did you want see me again ");
+
+// } while (userInput == true)
+
+
+// pratice set 4
+
+// var age = prompt("Enter your age ? ");
+// if (age > 4) {
+//   location.href = "https://www.google.com/"
+// }
+
+// pratice set 5
+
+// var color = prompt("Enter color name")
+
+// document.body.style.backgroundColor = color;
