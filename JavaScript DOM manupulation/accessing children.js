9f3cb3efa1e08baaa0bfa2bcc7e666b8c08782b5
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-10-17 09:11:50 +0530

    DOM modeling to access children

---
+++ JavaScript DOM manupulation/accessing children.js
@@ -0,0 +1,4 @@
+// console.log("Hello world")
+console.log(document.body.firstElementChild)
+    //  firstElementchild will give only tag not give comment,black text,text 
+console.log(document.body.firstChild)
