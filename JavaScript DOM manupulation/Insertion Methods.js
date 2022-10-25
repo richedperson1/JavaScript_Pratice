0ebc50b4d5d876d8fc793af90c298545cb6aa680
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-10-25 22:51:56 +0530

    modifing [Insertion Methods.html and Insertion Methods.js file ]

---
+++ JavaScript DOM manupulation/Insertion Methods.js
@@ -0,0 +1,23 @@
+let ele = document.getElementsByClassName("containersParent1")[0]
+
+let divRwq = "div"
+
+var newDiv = document.createElement(divRwq)
+
+// newDiv.innerHTML = "<div><h1> Hello world<h1></div>"
+
+// ele.prepend(newDiv)
+
+
+// for (let i = 0; i < 10; i++) {
+//     let newDiv = document.createElement(divRwq)
+//     newDiv.innerHTML = `<div><h1> Hello world ${soMany}<h1></div>`
+// }
+
+// ele.appendChild(newDiv)
+// ele.appendChild(newDiv)
+
+
+// newDiv.innerHTML = "<div><h1> Hello world<h1></div>"
+
+// ele.after(newDiv)
